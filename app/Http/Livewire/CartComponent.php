54879b833a3ee98f4ba33c9b14f09e714f8d7d59
salman608 <salman608@gmail.com>
2022-11-27 18:30:15 +0600

    Updated cart Item with Qty

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
 
 class CartComponent extends Component
 {
+
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId, $qty);
+    }
+
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId, $qty);
+    }
+
     public function render()
     {
         return view('livewire.cart-component');
