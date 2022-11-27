a278a3ef4c6a4f2bd248dc5241bfa92f66f5736f
salman608 <salman608@gmail.com>
2022-11-27 18:12:29 +0600

    add to cart function added with package

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -3,12 +3,19 @@
 namespace App\Http\Livewire;
 
 use App\Models\Product;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class ShopComponent extends Component
 {
     use WithPagination;
+    public function store($product_id, $product_name, $product_price)
+    {
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
+        session()->flash('success_message', 'Item Added to Cart');
+        return redirect()->route('shop.cart');
+    }
     public function render()
     {
         $products = Product::paginate(12);
