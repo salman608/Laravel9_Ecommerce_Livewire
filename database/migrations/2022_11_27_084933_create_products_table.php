3ea95d60c2cb31e69716653aa4c6bf3e897d57a6
salman608 <salman608@gmail.com>
2022-11-27 15:54:51 +0600

    shop page daynamic

--- database/migrations/2022_11_27_084933_create_products_table.php
+++ database/migrations/2022_11_27_084933_create_products_table.php
@@ -15,7 +15,21 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('short_description')->nullable();
+            $table->text('long_description')->nullable();
+            $table->decimal('regular_price', 8, 2);
+            $table->decimal('sale_price', 8, 2)->nullable();
+            $table->string('SKU');
+            $table->enum('stock_status', ['instock', 'outofstock']);
+            $table->boolean('featured')->default(false);
+            $table->unsignedInteger('quantity');
+            $table->string('image');
+            $table->text('images')->nullable();
+            $table->bigInteger('category_id')->unsigned()->nullable();
             $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
